e58dc306ee0c226bacff40af1627e6d3aa9834c9
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-16 17:52:33 -0400

    add python json example

---
+++ python_json_request.py
@@ -0,0 +1,10 @@
+# import json library
+import json # request url
+import urllib
+
+urlreq = 'https://groceries.asda.com/api/items/search?keyword=yogurt'# get response
+response = urllib.request.urlopen(urlreq)# load as json
+jresponse = json.load(response)# write to file as pretty print
+
+with open('asdaresp.json', 'w') as outfile:
+    json.dump(jresponse, outfile, sort_keys=True, indent=4)
