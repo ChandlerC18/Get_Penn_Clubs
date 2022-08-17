10a3cf7e141c995ccedb0d092fe034490abf5269
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-16 22:22:02 -0400

    reformat and comment penn clubs

--- get_all_penn_clubs.py
+++ get_all_penn_clubs.py
@@ -7,45 +7,45 @@ import pandas as pd
 from selenium.webdriver.firefox.options import Options
 #---------End of imports
 
-### FUNCTIONS ###
-
 ### MAIN FLOW ###
 urlpage = 'https://pennclubs.com'
 
 r = requests.get(urlpage)
-soup = BeautifulSoup(r.content, 'html.parser')
+soup = BeautifulSoup(r.content, 'html.parser') # get html of website
 content = soup.find('div', class_='pages__ResultsText-fxyd84-1 hAHPYs')
-num = int(content.text.split(' ')[1])
+num = int(content.text.split(' ')[1]) # get number of clubs
 
 options = Options()
-options.headless = True
+options.headless = True # option to show browser or not
 
-driver = webdriver.Firefox(options=options, executable_path='/Users/chandlercheung/Documents/Projects/geckodriver')
+driver = webdriver.Firefox(options=options, executable_path='/Users/chandlercheung/Documents/Projects/geckodriver') # start up web driver
+print("Started web driver")
 
-driver.get(urlpage)
+driver.get(urlpage) # load website
+
 
 for i in range(int(num / 15) + 2):
-    # execute script to scroll down the page
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;") # scroll down to bottom of page
+    time.sleep(3) # sleep for 3 sec
 
-    # sleep for 30s
-    time.sleep(2)
+print("Loaded website")
 
-clubs = driver.find_elements_by_xpath("//*[@class='ClubCard__CardWrapper-qqpg24-0 cKtqOz column is-half-desktop']//*[@class='ClubCard__Card-qqpg24-2 eufSiA card']//*[@class='ClubCard__CardHeader-qqpg24-4 guYdpv']//*[@class='ClubCard__CardTitle-qqpg24-5 igiPKV is-size-5']")
+clubs = driver.find_elements_by_xpath("//*[@class='ClubCard__CardWrapper-qqpg24-0 cKtqOz column is-half-desktop']//*[@class='ClubCard__Card-qqpg24-2 eufSiA card']//*[@class='ClubCard__CardHeader-qqpg24-4 guYdpv']//*[@class='ClubCard__CardTitle-qqpg24-5 igiPKV is-size-5']") # clubs
 
-links = driver.find_elements_by_xpath("//*[@class='ClubCard__CardWrapper-qqpg24-0 cKtqOz column is-half-desktop']//a[@href]")
+print("Got clubs")
+
+links = driver.find_elements_by_xpath("//*[@class='ClubCard__CardWrapper-qqpg24-0 cKtqOz column is-half-desktop']//a[@href]") # club websites
+
+print("Got website links")
 
 df = pd.DataFrame(columns=['Club', 'Website'])
 
 for i in range(len(clubs)):
     name = clubs[i].text
     link = links[i].get_attribute('href')
-    df.loc[len(df.index)] = [name, link]
+    df.loc[len(df.index)] = [name, link] # add data to csv
 
-print(len(clubs))
-print('________')
+df.to_csv('penn_clubs.csv', index=False)
+print("Saved csv file")
 
-# close driver
-driver.quit()
-if __name__ == '__main__':
-    pass
+driver.quit() # close driver
